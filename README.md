754a510831945a4d90b86d423245cd7a3066d048
alfiecg24 <alfiecockell@outlook.com>
2022-10-23 16:36:39 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SimpleDefaults
 
-A description of this package.
+## A simple way to save your classes and structs to UserDefaults
+
+SimpleDefaults enables a much easier method of saving custom objects into UserDefaults
--- Sources/SimpleDefaults/SimpleDefaults.swift
+++ Sources/SimpleDefaults/SimpleDefaults.swift
@@ -1,6 +1,39 @@
-public struct SimpleDefaults {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+protocol CustomDefaultsObject {
+    func setCustomObject<Object>(_ object: Object, forKey: String) throws where Object: Encodable
+    func getCustomObject<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable
+}
+
+enum CustomDefaultsObjectError: String, LocalizedError {
+    case unableToEncode = "Unable to encode object into data"
+    case noValue = "No data object found for the given key"
+    case unableToDecode = "Unable to decode object into given type"
+    
+    var errorDescription: String? {
+        rawValue
+    }
+}
+
+extension UserDefaults: CustomDefaultsObject {
+    func setCustomObject<Object>(_ object: Object, forKey: String) throws where Object: Encodable {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            set(data, forKey: forKey)
+        } catch {
+            throw CustomDefaultsObjectError.unableToEncode
+        }
+    }
+    
+    func getCustomObject<Object>(forKey: String, castTo type: Object.Type) throws -> Object where Object: Decodable {
+        guard let data = data(forKey: forKey) else { throw CustomDefaultsObjectError.noValue }
+        let decoder = JSONDecoder()
+        do {
+            let object = try decoder.decode(type, from: data)
+            return object
+        } catch {
+            throw CustomDefaultsObjectError.unableToDecode
+        }
     }
 }
