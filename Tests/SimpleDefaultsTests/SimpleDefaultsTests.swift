dd0908aa5544dfd137d61b9fa66a2ff94b59f9bf
alfiecg24 <alfiecockell@outlook.com>
2022-10-23 16:48:42 +0100

    Added tests

--- Tests/SimpleDefaultsTests/SimpleDefaultsTests.swift
+++ Tests/SimpleDefaultsTests/SimpleDefaultsTests.swift
@@ -1,8 +1,16 @@
 import XCTest
 @testable import SimpleDefaults
 
+struct Person: Codable {
+    var name: String
+    var age: Int
+}
+
 final class SimpleDefaultsTests: XCTestCase {
     func testExample() throws {
-        // Add your own tests here
+        let MyPerson = Person(name: "John", age: 30)
+        try UserDefaults.standard.setCustomObject(MyPerson, forKey: "MyPerson")
+        let Result = try UserDefaults.standard.getCustomObject(forKey: "MyPerson", castTo: Person.self)
+        print(Result)
     }
 }
